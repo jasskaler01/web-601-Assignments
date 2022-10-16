ad0cb2acf3a6360c45837aa2b806c392fdb6e603
Jaskaran Singh <w0782371@myscc.ca>
2022-10-16 19:46:47 -0400

    set temprature function and apply to button

--- assignment-1/heat-control/src/TempControlComponent.js
+++ assignment-1/heat-control/src/TempControlComponent.js
@@ -1,14 +1,27 @@
 import React, {useState} from "react";
 
 export default function TempControl(){
-    const [ temp , tempChange] = useState(30)
+    const [ temp , setTemp] = useState(30)
+    const [state , setState] = useState("Celsius")
+    function ChangeState(){
+        if (state === "Celsius"){
+            setTemp(temp+38.3)
+            setState("Fahrenheit")
+        }
+        if (state === "Fahrenheit"){
+            setTemp(temp-38.3)
+            setState("Celsius")
+        }
+    }
     return (
         <div>
             <h1>Jaskaran Singh</h1>
             <h2>Temprature Converter App</h2>
             <br/>
            <div>
-            <h3>Current Temperature : {temp} </h3>
+            <h3>Current Temperature : {temp} {state} </h3>
+          <button onClick={()=>{ChangeState()
+        }}>Change To {state}</button>
             </div> 
             </div>
        
