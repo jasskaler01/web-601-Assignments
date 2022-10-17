be7b82f634cdbc6f3fe7c0bab296790874313f95
Jaskaran Singh <w0782371@myscc.ca>
2022-10-16 21:00:59 -0400

    set some issues and update functions

--- assignment-1/heat-control/src/TempControlComponent.js
+++ assignment-1/heat-control/src/TempControlComponent.js
@@ -1,29 +1,42 @@
 import React, {useState} from "react";
-
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 export default function TempControl(){
     const [ temp , setTemp] = useState(30)
-    const [state , setState] = useState("Celsius")
-    function ChangeState(){
-        if (state === "Celsius"){
+    const [cel , setCel] = useState("Celsius")
+    const [fah , setFah] = useState("")
+    function ChangeF(){
+        if (cel === "Celsius"){
             setTemp(temp+38.3)
-            setState("Fahrenheit")
-        }
-        if (state === "Fahrenheit"){
+            setFah("Fahrenheit")
+            setCel("")
+        }}
+        function ChangeC(){
+        if (cel === ""){
             setTemp(temp-38.3)
-            setState("Celsius")
-        }
-    }
+            setCel("Celsius")
+            setFah("")
+        }}
+    
     return (
+        <Container maxWidth="sm">
         <div>
+            
             <h1>Jaskaran Singh</h1>
             <h2>Temprature Converter App</h2>
             <br/>
-           <div>
-            <h3>Current Temperature : {temp} {state} </h3>
-          <button onClick={()=>{ChangeState()
-        }}>Change To {state}</button>
+           <div >
+            <h3>Current Temperature :<button onClick={()=>{setTemp(temp-1)}}>-</button> {temp} <button onClick={()=>{setTemp(temp+1)}}>+</button> {cel}{fah} </h3>
+          <button onClick={()=>{ChangeF()
+        }}>Change To Fahrenheit</button>
+        <br/>
+        <button onClick={()=>{ChangeC()
+        }}>Change To Celsius</button>
+
             </div> 
+            
+            
             </div>
        
-    )
+            </Container>)
 }
