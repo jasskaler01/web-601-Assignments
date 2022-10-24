221b064a8afcc63257142be4e73db829d1f438c5
Jaskaran Singh <w0782371@myscc.ca>
2022-10-23 23:04:09 -0400

    add react icon pakages

--- assignment-1/my-app/src/Movie.js
+++ assignment-1/my-app/src/Movie.js
@@ -1,19 +1,22 @@
 import { useSlotProps } from "@mui/base";
 import React, {useState} from "react";
+import { Card } from a;
 
 export default function Movie(props){
 const [movieAge , setAge]= useState(2022-props.year)
     return(
         <div>
-        <hr />
+        <Card variant="outlined">
         <h2>{props.title}</h2>
         <h2>{props.year}</h2>
         <h2>{props.genre}</h2>
         <h2>{props.director}</h2>
         <h2>{movieAge}</h2>
-        <hr />
+        
+        </Card>
         </div>
         
     )
 
 }
+
